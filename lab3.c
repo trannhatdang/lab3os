a66ab075c1b6a060cce4e2a50178f66379fe2162
trannhatdang <77575859+trannhatdang@users.noreply.github.com>
2025-04-11 16:27:14 +0700

    update lab

--- lab3.c
+++ lab3.c
@@ -1,12 +1,33 @@
-int is_safe() {
-	//TODO
-	
-	
-	if(!found)
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <float.h>
+#include <pthread.h>
+
+bool break = false;
+
+void dummy_func()
+{
+	char[200] input;
+	while(fgets(input, 200, stdin))
+	{
+		pthread_mutex_lock(&lock);
+		if(strcmp(input, "err"))
+		{
+			break = true;
+		}
+		pthread_mutex_unlock(&lock);
+	}
+	printf("\nexiting...");
+}
+
+
+int is_safe() 
+{	
+	if(!break)
 	{
 		return -1;
 	}
-
 }
 
 int periodical_detector(void *arg)
@@ -14,19 +35,18 @@ int periodical_detector(void *arg)
 	while(1) {
 		sleep(5);
 		pthread_mutex_lock(&lock);
-		if(!is_safe())
-		{
-			if(!finished)
-			{
-				break;
-			}
+		if(is_safe() == -1)
+		{	
+			pthread_mutex_unlock(&lock);
+			break;
 		}
+		pthread_mutex_unlock(&lock);
 	}
-	return 0;
 }
 
 
 int main()
 {
+
 
 }
